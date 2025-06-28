ee69be2d7fa06ab549b6623ffa7e41784c5f9b7b
mf-rl <mf.ramirezl@gmail.com>
2025-06-27 22:01:54 -0500

    CompositeExample added

---
+++ Design.Patterns.Demo/2.Structural/C.Composite/Leaf.cs
@@ -0,0 +1,12 @@
+﻿namespace Design.Patterns.Demo._2.Structural.C.Composite
+{
+    public class Leaf : Component
+    {
+        public Leaf(string name) : base(name) { }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new string('-', depth) + Name);
+        }
+    }
+}
